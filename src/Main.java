bb70480ebd1e77a8185cb5a087e6a876112ce0df
brutskou <rutkovba@gmail.com>
2026-03-10 18:32:23 +0400

    Create an object of the Compony class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import model.Company;
+
 public class Main {
 
     public static void main(String[] args) {
         // git
         // GitHub
         // jsmith / johnsmith
+
+        Company company = new Company();
     }
 }
